c8305fa05ec43969f6f830d202aebdeb7ef741e4
Varun Shah <varun.shah196@gmail.com>
2018-10-01 19:26:14 -0400

    Verify login before each and every controller's actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def verify_logged_in
+    if request.fullpath != login_path && session[:logged_in] != "true"
+      redirect_to login_path, notice:"Please log in first"
+    end
+  end
+
+  before_action :verify_logged_in
 end
